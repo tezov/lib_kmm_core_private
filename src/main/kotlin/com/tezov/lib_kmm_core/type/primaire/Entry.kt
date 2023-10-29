d9807192166a8b44bdaf5f5c4a290c873388c2a8
tezov <tezov.app@gmail.com>
2023-10-29 11:01:09 +0100

    remove useless pure kmm

--- src/main/kotlin/com/tezov/lib_kmm_core/type/primaire/Entry.kt
+++
@@ -1,5 +0,0 @@
-
-
-package com.tezov.lib_kmm_core.type.primaire
-
-data class Entry<KEY, VALUE>(var key: KEY, var value: VALUE)
--- src/main/kotlin/com/tezov/lib_kmm_core/type/primaire/Scale.kt
+++
@@ -1,5 +0,0 @@
-
-
-package com.tezov.lib_kmm_core.type.primaire
-
-class Scale (var w: Float = 0f, var h: Float = 0f)
